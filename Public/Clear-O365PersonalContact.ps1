e57908d19488bcfd5ad1b1dc957a1d460f71ead6
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-22 11:26:38 +0100

    Add clear function

---
+++ Public/Clear-O365PersonalContact.ps1
@@ -0,0 +1,19 @@
+﻿function Clear-O365PersonalContact {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Mandatory)][string] $Identity,
+        [switch] $All
+    )
+    $CurrentContacts = Get-MgUserContact -UserId $Identity -All
+    foreach ($Contact in $CurrentContacts) {
+        $Guid = [guid]::Empty
+        $ConversionWorked = [guid]::TryParse($Contact.FileAs, [ref]$Guid)
+        if (-not $ConversionWorked) {
+            if (-not $All) {
+                Write-Warning -Message "Contact $($Contact.Id) is not created as part of O365Synchronizer. Skipping."
+                continue
+            }
+        }
+        Remove-MgUserContact -UserId $Identity -ContactId $Contact.Id
+    }
+}
