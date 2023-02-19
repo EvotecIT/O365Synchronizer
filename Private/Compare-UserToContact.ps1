7093b09801e88d0d00d93e4ca13d7bdf1c76a1b3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-02-19 18:33:36 +0100

    Rearranged code

--- Private/Compare-UserToContact.ps1
+++ Private/Compare-UserToContact.ps1
@@ -4,49 +4,36 @@
         [PSCustomObject] $ExistingContact,
         [PSCustomObject] $Contact
     )
-    $Properties = @(
-        'NickName'
-        'DisplayName'
-        'GivenName'
-        'Surname'
-        'EmailAddresses'
-        'MobilePhone'
-        'HomePhone'
-        'BusinessPhones'
-        'CompanyName'
-        'JobTitle'
-        'EmployeeId'
-        'Country'
-        'City'
-        'State'
-        'Street'
-        'PostalCode'
-    )
-
     $MappingContactToUser = [ordered] @{
         'MailNickname'   = 'NickName'
         'DisplayName'    = 'DisplayName'
         'GivenName'      = 'GivenName'
         'Surname'        = 'Surname'
-        'Mail'           = 'EmailAddresses'
+        # special treatment for 'Mail' because it's an array
+        'Mail'           = 'EmailAddresses.Address'
         'MobilePhone'    = 'MobilePhone'
         'HomePhone'      = 'HomePhone'
         'CompanyName'    = 'CompanyName'
         'BusinessPhones' = 'BusinessPhones'
         'JobTitle'       = 'JobTitle'
-        'Country'        = 'Country'
-        'City'           = 'City'
-        'State'          = 'State'
-        'Street'         = 'Street'
-        'PostalCode'     = 'PostalCode'
-        'EmployeeId'     = 'EmployeeId'
+        'Country'        = 'BusinessAddress.CountryOrRegion'
+        'City'           = 'BusinessAddress.City'
+        'State'          = 'BusinessAddress.State'
+        'Street'         = 'BusinessAddress.Street'
+        'PostalCode'     = 'BusinessAddress.PostalCode'
     }
     if ($Contact.PSObject.Properties.Name -contains 'MailNickName') {
         $TranslatedContact = $Contact
     } elseif ($Contact.PSObject.Properties.Name -contains 'Nickname') {
         $TranslatedContact = [ordered] @{}
         foreach ($Property in $MappingContactToUser.Keys) {
-            $TranslatedContact[$Property] = $Contact.$($MappingContactToUser[$Property])
+            if ($Property -eq 'Mail') {
+                $TranslatedContact[$Property] = $Contact.EmailAddresses | ForEach-Object { $_.Address }
+            } elseif ($MappingContactToUser[$Property] -like "*.*") {
+                $TranslatedContact[$Property] = $Contact.$($MappingContactToUser[$Property].Split('.')[0]).$($MappingContactToUser[$Property].Split('.')[1])
+            } else {
+                $TranslatedContact[$Property] = $Contact.$($MappingContactToUser[$Property])
+            }
         }
     } else {
         throw "Compare-UserToContact - Unknown user object $($ExistingContact.PSObject.Properties.Name)"
@@ -54,10 +41,17 @@
 
     $SkippedProperties = [System.Collections.Generic.List[string]]::new()
     $UpdateProperties = [System.Collections.Generic.List[string]]::new()
-    foreach ($Property in $Properties) {
-        if ([string]::IsNullOrEmpty($ExistingContact.$Property) -and [string]::IsNullOrEmpty($Contact.$Property)) {
+    foreach ($Property in $MappingContactToUser.Keys) {
+        if ([string]::IsNullOrEmpty($ExistingContact.$Property) -and [string]::IsNullOrEmpty($TranslatedContact.$Property)) {
             $SkippedProperties.Add($Property)
         } else {
+            # $TemporaryComparison = [ordered] @{
+            #     Name         = $Property
+            #     UserValue    = $ExistingContact.$Property
+            #     ContactValue = $TranslatedContact.$Property
+            # }
+            # $TemporaryComparison | ConvertTo-Json | Write-Verbose
+
             if ($User.$Property -ne $TranslatedContact.$Property) {
                 Write-Verbose -Message "Compare-UserToContact - Property $($Property) for $($ExistingContact.DisplayName) / $($ExistingContact.Mail) different ($($ExistingContact.$Property) vs $($Contact.$Property))"
                 $UpdateProperties.Add($Property)
