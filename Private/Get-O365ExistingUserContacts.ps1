93b6a4966f399048b7d1beeda71d77446ffd970d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-02-19 18:33:23 +0100

    Rearranged code

---
+++ Private/Get-O365ExistingUserContacts.ps1
@@ -0,0 +1,34 @@
+﻿function Get-O365ExistingUserContacts {
+    [cmdletbinding()]
+    param(
+        [string] $UserID,
+        [string] $GuidPrefix
+    )
+    # Lets get all contacts of given person and cache them
+    $ExistingContacts = [ordered] @{}
+    $CurrentContacts = Get-MgUserContact -UserId $UserId -All
+    foreach ($Contact in $CurrentContacts) {
+        if (-not $Contact.FileAs) {
+            continue
+        }
+
+        if ($GuidPrefix -and -not $Contact.FileAs.StartsWith($GuidPrefix)) {
+            continue
+        } elseif ($GuidPrefix -and $Contact.FileAs.StartsWith($GuidPrefix)) {
+            $Contact.FileAs = $Contact.FileAs.Substring($GuidPrefix.Length)
+        }
+
+        $Guid = [guid]::Empty
+        $ConversionWorked = [guid]::TryParse($Contact.FileAs, [ref]$Guid)
+        if (-not $ConversionWorked) {
+            continue
+        }
+
+        $Entry = [string]::Concat($Contact.FileAs)
+        $ExistingContacts[$Entry] = $Contact
+    }
+
+    Write-Color -Text "[i] ", "User ", $UserId, " has ", $CurrentContacts.Count, " contacts, out of which ", $ExistingContacts.Count, " synchronized." -Color Yellow, White, Cyan, White, Cyan, White, Cyan, White
+    Write-Color -Text "[i] ", "Users to process: ", $ExistingUsers.Count, " Contacts to process: ", $ExistingContacts.Count -Color Yellow, White, Cyan, White, Cyan
+    return $ExistingContacts
+}
