42cb27b963341bdf917ebfab9d731677192e57b4
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-02-19 18:33:26 +0100

    Rearranged code

---
+++ Private/Initialize-DefaultValuesO365.ps1
@@ -0,0 +1,63 @@
+﻿function Initialize-DefaultValuesO365 {
+    [cmdletBinding()]
+    param(
+
+    )
+
+    $Script:PropertiesUsers = @(
+        'DisplayName'
+        'GivenName'
+        'Surname'
+        'Mail'
+        'Nickname'
+        'MobilePhone'
+        'HomePhone'
+        'BusinessPhones'
+        'UserPrincipalName'
+        'Id',
+        'UserType'
+        'EmployeeType'
+        'AccountEnabled'
+        'CreatedDateTime'
+        'AssignedLicenses'
+
+        'MobilePhone'
+        'HomePhone'
+        'BusinessPhones'
+        'CompanyName'
+        'JobTitle'
+        'EmployeeId'
+        'Country'
+        'City'
+        'State'
+        'Street'
+        'PostalCode'
+    )
+
+    $Script:PropertiesContacts = @(
+        'DisplayName'
+        'GivenName'
+        'Surname'
+        'Mail'
+        'JobTitle'
+        'MailNickname'
+        #'Phones'
+        'UserPrincipalName'
+        'Id',
+        'CompanyName'
+        'OnPremisesSyncEnabled'
+        'Addresses'
+
+        'MobilePhone'
+        'HomePhone'
+        'BusinessPhones'
+        'CompanyName'
+        'JobTitle'
+        'EmployeeId'
+        'Country'
+        'City'
+        'State'
+        'Street'
+        'PostalCode'
+    )
+}
