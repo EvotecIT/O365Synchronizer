00fdf2ea5cc68a6dbae1cb626debfbbd7aaaaf2f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-02-19 10:57:36 +0100

    Add error handling

--- Private/Set-O365Contact.ps1
+++ Private/Set-O365Contact.ps1
@@ -11,6 +11,10 @@
         foreach ($Property in $Properties) {
             $PropertiesToUpdate[$Property] = $User.$Property
         }
-        Update-MgUserContact -UserId $UserID -ContactId $Contact.Id @PropertiesToUpdate -WhatIf:$WhatIfPreference
+        try {
+            Update-MgUserContact -UserId $UserID -ContactId $Contact.Id @PropertiesToUpdate -WhatIf:$WhatIfPreference -ErrorAction Stop
+        } catch {
+            Write-Color -Text "[!] ", "Failed to update contact for ", $User.DisplayName, " / ", $User.Mail, " because: ", $_.Exception.Message -Color Yellow, White, Red, White, Red, White, Red
+        }
     }
 }
