7103fe2b02f692ab5de77df914fdec62c6fe6b9f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-09-24 23:06:21 +0200

    Add New-O365OrgContact function and handle creation of O365 organization contact.
    The code adds a new PowerShell function called New-O365OrgContact that handles the creation of an Office 365 organization contact. It includes a param block with a single parameter called Source. The function uses the Write-Color function to output a message indicating that a contact is being added. It then tries to create a new mail contact using the New-MailContact cmdlet, passing in the values from the Source parameter. If the creation fails, an error message is displayed. Finally, if the contact is successfully created, it calls the Set-O365OrgContact function to further process the contact.

---
+++ Private/New-O365OrgContact.ps1
@@ -0,0 +1,15 @@
+﻿function New-O365OrgContact {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Object] $Source
+    )
+    Write-Color -Text "[+] ", "Adding ", $Source.DisplayName, " / ", $Source.PrimarySmtpAddress -Color Yellow, White, Cyan, White, Cyan
+    try {
+        $Created = New-MailContact -DisplayName $Source.DisplayName -ExternalEmailAddress $Source.PrimarySmtpAddress -Name $Source.Name -WhatIf:$WhatIfPreference -ErrorAction Stop
+    } catch {
+        Write-Color -Text "[e] ", "Failed to create contact. Error: ", ($_.Exception.Message -replace ([Environment]::NewLine), " " )-Color Yellow, White, Red
+    }
+    if ($Created) {
+        Set-O365OrgContact -MailContact $Created -Contact @{} -Source $Source -SourceContact $SourceContact
+    }
+}
