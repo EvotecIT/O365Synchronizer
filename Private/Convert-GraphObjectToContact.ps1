8d3635447bcf5d0189e2e7ac60b78b2ad765d018
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-09-24 23:05:30 +0200

    Add function to convert Graph objects to contact objects
    This commit adds a new function `Convert-GraphObjectToContact` which takes a Graph object as input and converts it into separate contact and mail contact objects. The function uses ordered dictionaries to map properties from the input object to the corresponding properties of the contact and mail contact objects. The converted objects are then returned as output.

---
+++ Private/Convert-GraphObjectToContact.ps1
@@ -0,0 +1,60 @@
+﻿function Convert-GraphObjectToContact {
+    [cmdletbinding()]
+    param(
+        $SourceObject
+    )
+
+    $MappingMailContact = [ordered] @{
+        DisplayName               = 'DisplayName'
+        Name                      = 'DisplayName'
+        PrimarySmtpAddress        = 'Mail'
+        CustomAttribute1          = 'CustomAttribute1'
+        CustomAttribute2          = 'CustomAttribute2'
+        ExtensionCustomAttribute1 = 'ExtensionCustomAttribute1'
+        #HiddenFromAddressListsEnabled = 'HiddenFromAddressListsEnabled'
+    }
+    $MappingContact = [ordered] @{
+        DisplayName         = 'DisplayName'
+        Name                = 'DisplayName'
+        WindowsEmailAddress = 'Mail'
+        Title               = 'JobTitle'
+        FirstName           = 'GivenName'
+        LastName            = 'SurName'
+        HomePhone           = 'HomePhone'
+        MobilePhone         = 'MobilePhone'
+        Phone               = 'BusinessPhones'
+        CompanyName         = 'CompanyName'
+        Department          = 'Department'
+        Office              = 'Office'
+        StreetAddress       = 'StreetAddress'
+        City                = 'City'
+        StateOrProvince     = 'StateOrProvince'
+        PostalCode          = 'PostalCode'
+        CountryOrRegion     = 'CountryOrRegion'
+        #Fax                 = 'Fax'
+    }
+
+    $NewContact = [ordered] @{}
+    foreach ($Property in $MappingContact.Keys) {
+        $PropertyName = $MappingContact[$Property]
+        if ($PropertyName -eq 'BusinessPhones') {
+            $NewContact[$Property] = [string] $SourceObject.$PropertyName
+        } else {
+            $NewContact[$Property] = $SourceObject.$PropertyName
+        }
+    }
+    $NewMailContact = [ordered] @{}
+    foreach ($Property in $MappingMailContact.Keys) {
+        $PropertyName = $MappingMailContact[$Property]
+        #if ($PropertyName -eq 'BusinessPhones') {
+        #    $NewMailContact[$Property] = [string] $SourceObject.$PropertyName
+        # } else {
+        $NewMailContact[$Property] = $SourceObject.$PropertyName
+        # }
+    }
+    $Output = [ordered] @{
+        Contact     = [PSCustomObject] $NewContact
+        MailContact = [PSCustomObject] $NewMailContact
+    }
+    $Output
+}
