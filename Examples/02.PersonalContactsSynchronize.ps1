9ec770ca6f47b9c333ce242755a1f92e61cd7e7b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-12-02 19:57:32 +0100

    Update example

--- Examples/02.PersonalContactsSynchronize.ps1
+++ Examples/02.PersonalContactsSynchronize.ps1
@@ -4,8 +4,9 @@ $ClientID = '9e1b3c'
 $TenantID = 'ceb371f6'
 $ClientSecret = 'nQF8Q'
 
+# connect to Microsoft Graph API
 $Credentials = [pscredential]::new($ClientID, (ConvertTo-SecureString $ClientSecret -AsPlainText -Force))
 Connect-MgGraph -ClientSecretCredential $Credentials -TenantId $TenantID -NoWelcome
 
 # synchronize contacts for two users of two types (Member, Contact) using GUID prefix
-Sync-O365PersonalContact -UserId 'test@evotec.pl', 'test1@evotec.pl' -Verbose -MemberTypes 'Member','Contact' -GuidPrefix 'O365Synchronizer' -WhatIf | Format-Table *
+Sync-O365PersonalContact -UserId 'test@evotec.pl', 'test1@evotec.pl' -Verbose -MemberTypes 'Member', 'Contact' -GuidPrefix 'O365Synchronizer' -WhatIf | Format-Table *
