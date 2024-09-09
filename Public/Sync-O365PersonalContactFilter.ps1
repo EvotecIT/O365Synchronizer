8afe17f0b043c11032079fc5a471d1f49af14d2a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-09-09 16:44:26 +0200

    Add filtering by group, property

---
+++ Public/Sync-O365PersonalContactFilter.ps1
@@ -0,0 +1,47 @@
+﻿function Sync-O365PersonalContactFilter {
+    <#
+    .SYNOPSIS
+    Provides a way to filter out users/contacts based on properties.
+
+    .DESCRIPTION
+    Provides a way to filter out users/contacts based on properties.
+    Only users/contacts that match the filter will be included/excluded.
+
+    .PARAMETER Type
+    Type of the filter. It can be 'Include' or 'Exclude'.
+
+    .PARAMETER Operator
+    Operator to use. It can be 'Equal', 'NotEqual', 'LessThan', 'MoreThan', 'Like'.
+
+    .PARAMETER Property
+    Property to use for comparison. Keep in mind that it has to exists on the object.
+
+    .PARAMETER Value
+    Value to compare against. It can be single value or multiple values.
+
+    .EXAMPLE
+    Sync-O365PersonalContact -UserId 'przemyslaw.klys@test.pl' -Verbose -MemberTypes 'Contact', 'Member' -GuidPrefix 'O365Synchronizer' -WhatIf -PassThru {
+        Sync-O365PersonalContactFilter -Type Include -Property 'CompanyName' -Value 'OtherCompany*','Evotec*' -Operator 'like' # filter out on CompanyName
+        Sync-O365PersonalContactFilterGroup -Type Include -GroupID 'e7772951-4b0e-4f10-8f38-eae9b8f55962' # filter out on GroupID
+    } | Format-Table
+
+    .NOTES
+    General notes
+    #>
+    [cmdletBinding()]
+    param(
+        [ValidateSet('Include', 'Exclude')][Parameter(Mandatory)][string] $Type,
+        [ValidateSet('Equal', 'NotEqual', 'LessThan', 'MoreThan', 'Like')][Parameter(Mandatory)][string] $Operator,
+        [Parameter(Mandatory)][string] $Property,
+        [Parameter()][Object] $Value
+    )
+
+    $Filter = [ordered] @{
+        FilterType = 'Property'
+        Type       = $Type
+        Operator   = $Operator
+        Property   = $Property
+        Value      = $Value
+    }
+    $Filter
+}
---
+++ Public/Sync-O365PersonalContactFilterGroup.ps1
@@ -0,0 +1,37 @@
+﻿function Sync-O365PersonalContactFilterGroup {
+    <#
+    .SYNOPSIS
+    Provides a way to filter out users/contacts based on groups.
+
+    .DESCRIPTION
+    Provides a way to filter out users/contacts based on groups.
+    Only users/contacts that are part of the group will be included/excluded.
+
+    .PARAMETER Type
+    Type of the filter. It can be 'Include' or 'Exclude'.
+
+    .PARAMETER GroupID
+    One or multiple GroupID's to filter out users/contacts. Keep in mind that it's not the name of the group, but the actual ID of the group.
+    Due to performance reasons it's better to use GroupID instead of GroupName.
+
+    .EXAMPLE
+    Sync-O365PersonalContact -UserId 'przemyslaw.klys@test.pl' -Verbose -MemberTypes 'Contact', 'Member' -GuidPrefix 'O365Synchronizer' -WhatIf -PassThru {
+        Sync-O365PersonalContactFilter -Type Include -Property 'CompanyName' -Value 'OtherCompany*','Evotec*' -Operator 'like' # filter out on CompanyName
+        Sync-O365PersonalContactFilterGroup -Type Include -GroupID 'e7772951-4b0e-4f10-8f38-eae9b8f55962' # filter out on GroupID
+    } | Format-Table
+
+    .NOTES
+    General notes
+    #>
+    [cmdletBinding()]
+    param(
+        [ValidateSet('Include', 'Exclude')][Parameter(Mandatory)][string] $Type,
+        [Parameter(Mandatory)][string[]] $GroupID
+    )
+    $Filter = [ordered] @{
+        FilterType = 'Group'
+        Type       = $Type
+        GroupID    = $GroupID
+    }
+    $Filter
+}
