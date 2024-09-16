8a1b6e353d95b53b31ef8e27b3676b30ff7bf396
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-09-16 12:05:57 +0200

    add some clarification for easier code reading

--- Private/Set-O365InternalContact.ps1
+++ Private/Set-O365InternalContact.ps1
@@ -1,4 +1,26 @@
 ﻿function Set-O365InternalContact {
+    <#
+    .SYNOPSIS
+    Short description
+
+    .DESCRIPTION
+    Long description
+
+    .PARAMETER UserID
+    Identity of the user to synchronize contacts to. It can be UserID or UserPrincipalName.
+
+    .PARAMETER User
+    User/Contact object from GAL
+
+    .PARAMETER Contact
+    Existing contact in user's personal contacts
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    General notes
+    #>
     [CmdletBinding(SupportsShouldProcess)]
     param(
         [string] $UserID,
@@ -6,7 +28,7 @@
         [PSCustomObject] $Contact
     )
 
-    $OutputObject = Compare-UserToContact -ExistingContact $User -Contact $Contact -UserID $UserID
+    $OutputObject = Compare-UserToContact -ExistingContactGAL $User -Contact $Contact -UserID $UserID
     if ($OutputObject.Update.Count -gt 0) {
         if ($User.Mail) {
             Write-Color -Text "[i] ", "Updating ", $User.DisplayName, " / ", $User.Mail, " properties to update: ", $($OutputObject.Update -join ', '), " properties to skip: ", $($OutputObject.Skip -join ', ') -Color Yellow, White, Green, White, Green, White, Green, White, Cyan
