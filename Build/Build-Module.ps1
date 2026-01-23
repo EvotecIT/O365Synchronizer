b6414e177333f68a9caf3fc2395d616ce25bb22a
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-01-23 20:40:41 +0100

    feat(build): ✨ enhance module build configuration
    * Added `RefreshPSD1Only` option to the build configuration.
    * Removed unnecessary `Clear-Host` command for cleaner execution.

--- Build/Build-Module.ps1
+++ Build/Build-Module.ps1
@@ -1,5 +1,3 @@
-Clear-Host
-
 Invoke-ModuleBuild -ModuleName 'O365Synchronizer' {
     # Usual defaults as per standard module
     $Manifest = [ordered] @{
@@ -101,7 +99,16 @@ Invoke-ModuleBuild -ModuleName 'O365Synchronizer' {
 
     New-ConfigurationImportModule -ImportSelf
 
-    New-ConfigurationBuild -Enable:$true -SignModule -MergeModuleOnBuild -MergeFunctionsFromApprovedModules -CertificateThumbprint '483292C9E317AA13B07BB7A96AE9D1A5ED9E7703'
+    $newConfigurationBuildSplat = @{
+        Enable                            = $true
+        SignModule                        = $true
+        MergeModuleOnBuild                = $true
+        MergeFunctionsFromApprovedModules = $true
+        CertificateThumbprint             = '483292C9E317AA13B07BB7A96AE9D1A5ED9E7703'
+        RefreshPSD1Only                   = $true
+    }
+
+    New-ConfigurationBuild @newConfigurationBuildSplat
 
     #New-ConfigurationTest -TestsPath "$PSScriptRoot\..\Tests" -Enable
 
